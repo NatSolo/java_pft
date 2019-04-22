e6fa7d20a237a171a3e406595242d21efa0e4a30
NatSolo <solodkovanvtest@gmail.com>
2019-04-22 13:53:07 -0400

    New classes was created. TestNG was connected.

--- sendbox/src/main/java/ru/stqa/pft/sedbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sedbox/MyFirstProgram.java
@@ -6,24 +6,23 @@ public class MyFirstProgram {
     hello("world");
     hello("user");
     hello("Natalia");
-    double l = 5;
-    System.out.println("Square area with each side "+ l + " = " + area(l));
-    double a = 4;
-    double b = 6;
-    System.out.println("Rectangle area with sides " + a + "and " + b + " =" + area(a,b));
+
+    Square s = new Square(5);
+
+    System.out.println("Square area with each side "+ s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4,6);
+
+    System.out.println("Rectangle area with sides " + r.a + "and " + r.b + " =" + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello," + somebody + "!");
   }
 
-    public static double area(double len) {
-      return len*len;
-    }
 
-    public static double area(double a, double b) {
-    return a*b;
-    }
+
+
   }
 
 
